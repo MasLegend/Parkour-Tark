9536b4f2f324a8ddffd747e87e70b7cb7b5ac8e5
MasLegend <maslegend@users.noreply.github.com>
2020-10-16 22:33:23 +0200

    0.1.1
    Need to improve:
    Wallrun, Grappling collide

--- Assets/Scripts/GrapplingGun.cs
+++ Assets/Scripts/GrapplingGun.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class GrapplingGun : MonoBehaviour
@@ -6,12 +7,17 @@ public class GrapplingGun : MonoBehaviour
     private LineRenderer lr;
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
+    public LayerMask whatBlocksGrapple;
     public Transform gunTip, camera, player;
     private float maxDistance = 100f;
     private SpringJoint joint;
     private int NumberPush;
     public int MaxPush;
     public float pullForce;
+    public GameObject bullet;
+    public float speed = 100f;
+    private RaycastHit hit1;
+    private RaycastHit hit2;
 
     void Awake()
     {
@@ -22,33 +28,49 @@ public class GrapplingGun : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            StartGrapple();
+            if (canGrapple())
+            {
+                StartGrapple();
+            }
         }
+
+        //condition to stop grappling if an object gets between
+        //the grapple point and the player (not finished)
+
+        //else if (Input.GetMouseButtonUp(0) || !stillCanGrapple()) 
         else if (Input.GetMouseButtonUp(0))
         {
             StopGrapple();
+
         }
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             CloseJoints();
+
         }
+
     }
 
-    //Called after Update
-    void LateUpdate()
-    {
-        DrawRope();
-    }
-
-    /// <summary>
-    /// Call whenever we want to start a grapple
-    /// </summary>
-    void StartGrapple()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable))
+        //Called after Update
+        void LateUpdate()
         {
-            grapplePoint = hit.point;
+            DrawRope();
+        }
+
+        /// <summary>
+        /// Call whenever we want to start a grapple
+        /// </summary>
+        void StartGrapple()
+        {
+            GameObject instaBullet = Instantiate(bullet, gunTip.position, Quaternion.identity) as GameObject;
+
+            Rigidbody instaBulletRigidBody = instaBullet.GetComponent<Rigidbody>();
+
+            instaBulletRigidBody.velocity = camera.forward * speed;
+
+            FindObjectOfType<AudioManager>().Play("GrapplingShot");
+
+            grapplePoint = hit1.point;
             joint = player.gameObject.AddComponent<SpringJoint>();
             joint.autoConfigureConnectedAnchor = false;
             joint.connectedAnchor = grapplePoint;
@@ -67,48 +89,50 @@ public class GrapplingGun : MonoBehaviour
             lr.positionCount = 2;
             currentGrapplePosition = gunTip.position;
 
+
         }
-    }
-    /// <summary>
-    /// Call whenever we want to get joint closer
-    /// </summary>
-    void CloseJoints()
-    {
-        if (NumberPush > MaxPush - 1)
+
+        /// <summary>
+        /// Call whenever we want to get joint closer
+        /// </summary>
+        void CloseJoints()
         {
-            return;
+            if (NumberPush > MaxPush - 1)
+            {
+                return;
+            }
+            if (joint)
+            {
+                NumberPush++;
+                joint.connectedAnchor = grapplePoint;
+
+                float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+                //The distance grapple will try to keep from grapple point. 
+
+                joint.maxDistance = 10f;
+                joint.minDistance = distanceFromPoint * 0.25f;
+
+                //Adjust these values to fit your game.
+                joint.spring = pullForce;
+                joint.damper = 7f;
+                joint.massScale = 4.5f;
+
+                lr.positionCount = 2;
+                currentGrapplePosition = gunTip.position;
+            }
         }
-        if (joint)
+
+        /// <summary>
+        /// Call whenever we want to stop a grapple
+        /// </summary>
+        void StopGrapple()
         {
-            NumberPush++;
-            joint.connectedAnchor = grapplePoint;
-
-            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
-
-            //The distance grapple will try to keep from grapple point. 
-
-            joint.maxDistance = 10f;
-            joint.minDistance = distanceFromPoint * 0.25f;
-
-            //Adjust these values to fit your game.
-            joint.spring = pullForce;
-            joint.damper = 7f;
-            joint.massScale = 4.5f;
-
-            lr.positionCount = 2;
-            currentGrapplePosition = gunTip.position;
+            lr.positionCount = 0;
+            NumberPush = 0;
+            Destroy(joint);
         }
-    }
-
-    /// <summary>
-    /// Call whenever we want to stop a grapple
-    /// </summary>
-    void StopGrapple()
-    {
-        lr.positionCount = 0;
-        NumberPush = 0;
-        Destroy(joint);
-    }
+    
 
     private Vector3 currentGrapplePosition;
 
@@ -132,4 +156,45 @@ public class GrapplingGun : MonoBehaviour
     {
         return grapplePoint;
     }
+
+    public bool canGrapple()
+    {
+        if (Physics.Raycast(camera.position, camera.forward, out hit1, maxDistance, whatIsGrappleable))
+        {
+            if (Physics.Raycast(camera.position, camera.forward, out hit2, maxDistance, whatBlocksGrapple))
+            {
+                if (hit1.distance < hit2.distance)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool stillCanGrapple()
+    {
+        Vector3 fromGun = gunTip.transform.position;
+        Vector3 direction = fromGun - grapplePoint;
+
+        if (Physics.Raycast(gunTip.position, direction, out hit1, maxDistance, whatIsGrappleable))
+        {
+            if (Physics.Raycast(gunTip.position, direction, out hit2, maxDistance, whatBlocksGrapple))
+            {
+                if (hit1.distance < hit2.distance)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField] private string selectableTag = "Selectable";
-    public Image img;
+    public UnityEngine.UI.Image img;
 
     void Start()
     {
@@ -24,10 +25,17 @@ public class SelectionManager : MonoBehaviour
             var selection = hit.transform;
             if (selection.CompareTag(selectableTag))
                 img.color = new Color(0f, 1f, 0f);
+            else
+            {
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    img.color = new Color(1f, 0f, 0f);
+                }
+                else
+                    img.color = new Color(1f, 1f, 1f);
+            }
         }
         else
-        {
             img.color = new Color(1f, 1f, 1f);
-        }
     }
 }
